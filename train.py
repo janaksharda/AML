5f213516376ed025eef5ebfb8a497d237fe761e3
janaksharda <41687598+janaksharda@users.noreply.github.com>
2020-02-20 16:13:33 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -11,27 +11,29 @@ from tqdm import tqdm
 
 from eval import eval_net
 from unet import UNet
-
+import pandas as pd
 from torch.utils.tensorboard import SummaryWriter
 from utils.dataset import BasicDataset
 from torch.utils.data import DataLoader, random_split
-
-dir_img = '/content/drive/My Drive/ML /Training/Damaged'
-dir_mask = 'data/masks/'
+from dice_loss import dice_coeff
+# dir_img = '/content/drive/My Drive/ML /Training/Damaged'
+# dir_mask = 'data/masks/'
 dir_checkpoint = 'checkpoints/'
-
+dir_img = '/content/drive/My Drive/ML /img2/ap/'
+dir_mask = '/content/drive/My Drive/ML /img2/ver/'
 
 def train_net(net,
               device,
-              epochs=5,
-              batch_size=1,
+              epochs=20,
+              batch_size=16,
               lr=0.1,
               val_percent=0.1,
               save_cp=True,
               img_scale=0.5):
-    lr = input()
-    momentum = input()
-    weight_decay = input()
+    momentum = 0.9
+    lr = 0.04*(1-momentum) #for RMSProp
+    weight_decay = 1e-10
+    lr_decay = 0.001
     dataset = BasicDataset(dir_img, dir_mask, img_scale)
     print(len(dataset))
     # for i in range(len(dataset.ids)):
@@ -39,8 +41,8 @@ def train_net(net,
     n_val = int(len(dataset) * val_percent)
     n_train = len(dataset) - n_val
     train, val = random_split(dataset, [n_train, n_val])
-    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
-    val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
+    train_loader = DataLoader(train, batch_size=1, shuffle=True, num_workers=1, pin_memory=True)
+    val_loader = DataLoader(val, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
 
     writer = SummaryWriter(comment=f'LR_{lr}_BS_{batch_size}_SCALE_{img_scale}')
     global_step = 0
@@ -56,16 +58,23 @@ def train_net(net,
         Images scaling:  {img_scale}
     ''')
 
-    optimizer = optim.RMSprop(net.parameters(), lr=lr, weight_decay=weight_decay,momentum=momentum)
+    optimizer = optim.RMSprop(net.parameters(), lr=lr, weight_decay=weight_decay,momentum=momentum,centered = True)
+    # optimizer = optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
+    # optimizer = optim.Adagrad(net.parameters(), lr=lr, weight_decay=weight_decay,lr_decay = lr_decay)
     if net.n_classes > 1:
         criterion = nn.CrossEntropyLoss()
     else:
-        criterion = nn.BCEWithLogitsLoss()
-
-    for epoch in range(epochs):
+        criterion = nn.MSELoss()
+    epoch_loss = 0
+    dice_epoch_loss = 0
+    i = 0
+    for epoch in range(20):
         net.train()
         print(epochs)
+        print(epoch_loss)
+        print(dice_epoch_loss)
         epoch_loss = 0
+        dice_epoch_loss = 0;
         batch_temp = 0;
         with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
             for batch in train_loader:
@@ -83,7 +92,20 @@ def train_net(net,
                 true_masks = true_masks.to(device=device, dtype=mask_type)
 
                 masks_pred = net(imgs)
-                loss = criterion(masks_pred, true_masks)
+                print(np.mean(masks_pred.cpu().data.numpy()))
+                pred = (masks_pred > 0.5).float()
+                dice_epoch_loss += dice_coeff(pred, true_masks.squeeze(dim=1)).item()
+                print(dice_coeff(pred, true_masks.squeeze(dim=1)).item())
+                temp1 = net.inc.double_conv
+                # if (epoch == 2):
+                #     image = pred[0,0,:,:].cpu().data.numpy()
+                #     df = pd.DataFrame(image) 
+                #     df.to_csv('/content/images1/pred'+str(i) +'.csv') 
+                #     i+=1
+
+                # print(temp1[0].weight)
+                # m = nn.Sigmoid()
+                loss = criterion((masks_pred), true_masks)
                 epoch_loss += loss.item()
                 writer.add_scalar('Loss/train', loss.item(), global_step)
 
@@ -95,7 +117,7 @@ def train_net(net,
 
                 pbar.update(imgs.shape[0])
                 global_step += 1
-                if global_step % (len(dataset) // (10 * batch_size)) == 0:
+                if global_step % (len(dataset) // (2)) == 0:
                     val_score = 0
                     for batch in val_loader:
                       val_score = val_score + eval_net(net, batch, device, 0)/n_val
@@ -122,19 +144,19 @@ def train_net(net,
                        dir_checkpoint + f'CP_epoch{epoch + 1}.pth')
             logging.info(f'Checkpoint {epoch + 1} saved !')
 
-    val_score = eval_net(net, val_loader, device, n_val)
-    if net.n_classes > 1:
-        logging.info('Validation cross entropy: {}'.format(val_score))
-        writer.add_scalar('Loss/test', val_score, global_step)
+    # val_score = eval_net(net, val_loader, device, n_val)
+    # if net.n_classes > 1:
+    #     logging.info('Validation cross entropy: {}'.format(val_score))
+    #     writer.add_scalar('Loss/test', val_score, global_step)
 
-    else:
-        logging.info('Validation Dice Coeff: {}'.format(val_score))
-        writer.add_scalar('Dice/test', val_score, global_step)
+    # else:
+    #     logging.info('Validation Dice Coeff: {}'.format(val_score))
+    #     writer.add_scalar('Dice/test', val_score, global_step)
 
-        writer.add_images('images', imgs, global_step)
-    if net.n_classes == 1:
-        writer.add_images('masks/true', true_masks, global_step)
-        writer.add_images('masks/pred', torch.sigmoid(masks_pred) > 0.5, global_step)
+    #     writer.add_images('images', imgs, global_step)
+    # if net.n_classes == 1:
+    #     writer.add_images('masks/true', true_masks, global_step)
+    #     writer.add_images('masks/pred', torch.sigmoid(masks_pred) > 0.5, global_step)
     writer.close()
 
 
